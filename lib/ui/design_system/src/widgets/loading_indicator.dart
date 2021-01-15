292db9471412de078091d5d2a6340b9604730204
Frank Moreno <frankmoreno1993@gmail.com>
2021-01-15 12:25:00 -0500

    refactor: Loader to LoaderIndicator

--- lib/ui/design_system/src/widgets/loading_indicator.dart
+++ lib/ui/design_system/src/widgets/loading_indicator.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_graphql_swapi/ui/design_system/design_system.dart';
 
-class Loader extends StatelessWidget {
-  const Loader({Key key}) : super(key: key);
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
