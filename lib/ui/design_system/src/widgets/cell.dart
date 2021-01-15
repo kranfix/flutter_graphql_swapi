938f4670e0be7ebec63ff8cef10d7338a9403c11
Frank Moreno <frankmoreno1993@gmail.com>
2021-01-15 13:38:27 -0500

    refactor: adding showDivider and trailing to Cell

--- lib/ui/design_system/src/widgets/cell.dart
+++ lib/ui/design_system/src/widgets/cell.dart
@@ -1,24 +1,69 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_graphql_swapi/ui/ui.dart';
 
 class Cell extends StatelessWidget {
   const Cell({
     Key key,
     this.alignment = Alignment.center,
+    this.internalHeight,
     this.child,
+    this.showDivider = false,
+    this.trailing,
   })  : assert(alignment != null),
+        assert(showDivider != null),
         super(key: key);
 
   final Alignment alignment;
   final Widget child;
+  final double internalHeight;
+  final bool showDivider;
+  final Widget trailing;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 52,
+    const dividerHeight = 1.0;
+    const padding = 16.0;
+
+    final _width = MediaQuery.of(context).size.width;
+    const trailingWidth = 36.0;
+    final internalWidth = trailing != null ? _width - trailingWidth : _width;
+
+    Widget tree = Container(
+      width: internalWidth,
+      height: internalHeight,
+      margin: const EdgeInsets.all(16),
       alignment: alignment,
-      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: child,
     );
+
+    if (trailing != null) {
+      tree = Row(
+        children: [
+          tree,
+          SizedBox(
+            width: trailingWidth,
+            height: internalHeight,
+            child: trailing,
+          ),
+        ],
+      );
+    }
+
+    if (showDivider) {
+      tree = Column(
+        children: [
+          tree,
+          Padding(
+            padding: const EdgeInsets.only(left: padding),
+            child: Divider(
+              color: AppColors.light,
+              height: dividerHeight,
+            ),
+          ),
+        ],
+      );
+    }
+    return tree;
   }
 }
