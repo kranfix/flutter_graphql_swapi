b5738423409c45b01d68d4dcaea3681a35a81397
Frank Moreno <frankmoreno1993@gmail.com>
2021-01-15 14:58:41 -0500

    refactor: cleaning Cell compositiong

--- lib/ui/design_system/src/widgets/cell.dart
+++ lib/ui/design_system/src/widgets/cell.dart
@@ -5,65 +5,54 @@ import 'package:flutter_graphql_swapi/ui/ui.dart';
 class Cell extends StatelessWidget {
   const Cell({
     Key key,
+    this.padding = const EdgeInsets.all(16),
     this.alignment = Alignment.center,
-    this.internalHeight,
     this.child,
     this.showDivider = false,
     this.trailing,
-  })  : assert(alignment != null),
+  })  : assert(padding != null),
+        assert(alignment != null),
         assert(showDivider != null),
         super(key: key);
 
+  final EdgeInsets padding;
   final Alignment alignment;
   final Widget child;
-  final double internalHeight;
   final bool showDivider;
   final Widget trailing;
 
   @override
   Widget build(BuildContext context) {
     const dividerHeight = 1.0;
-    const padding = 16.0;
 
     const trailingWidth = 36.0;
 
     Widget tree = Container(
-      width: double.infinity,
-      height: internalHeight,
-      margin: const EdgeInsets.all(16),
+      margin: padding,
       alignment: alignment,
       child: child,
     );
 
-    tree = SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: internalHeight != null ? internalHeight + 2 * padding : null,
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              height: internalHeight,
-              margin: const EdgeInsets.all(16),
-              alignment: alignment,
-              child: child,
-            ),
-          ),
-          if (trailing != null)
+    if (trailing != null) {
+      tree = SizedBox(
+        child: Row(
+          children: [
+            Expanded(child: tree),
             SizedBox(
               width: trailingWidth,
-              height: internalHeight,
               child: trailing,
             ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    }
 
     if (showDivider) {
       tree = Column(
         children: [
           tree,
           Padding(
-            padding: const EdgeInsets.only(left: padding),
+            padding: EdgeInsets.zero.copyWith(left: padding.left),
             child: Divider(
               color: AppColors.light,
               height: dividerHeight,
