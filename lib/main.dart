e2cf1ba998a287b50ef0a36dbb0d0f23df1d9354
Frank Moreno <frankmoreno1993@gmail.com>
2021-01-17 09:05:06 -0500

    chore: StarWars app

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_graphql_swapi/domain/domain.dart';
+import 'package:flutter_graphql_swapi/ui/ui.dart';
+import 'package:simple_gql/simple_gql.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider(
+          create: (_) => GQLClient(
+            url: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+          ),
+        ),
+        ChangeNotifierProxyProvider<GQLClient, PersonList>(
+          create: (ctx) => PersonList(ctx.read<GQLClient>()),
+          update: (ctx, gqlClient, personList) =>
+              personList..gqlClient = gqlClient,
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter GraphQL SWAPI',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
