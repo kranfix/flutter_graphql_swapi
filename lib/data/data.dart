94051643970f0c373417970d4c47f92b22eabac0
Frank Moreno <frankmoreno1993@gmail.com>
2021-01-17 08:31:35 -0500

    chore: Planet model

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -3,3 +3,4 @@ library data;
 export 'src/gpl_commons/gql_commons.dart';
 export 'src/vehicle/vehicle.dart';
 export 'src/person/person.dart';
+export 'src/planet/planet.dart';
---
+++ lib/data/src/planet/planet.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_graphql_swapi/data/data.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'planet.freezed.dart';
+part 'planet.g.dart';
+
+@freezed
+abstract class Planet with _$Planet {
+  const factory Planet({
+    @nullable String name,
+  }) = _Planet;
+
+  factory Planet.fromJson(Map<String, dynamic> json) => _$PlanetFromJson(json);
+}
